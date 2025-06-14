9414707f27caa8b0f86ed2290c4b155c80368ff2
unknown <tarasovlev7@gmail.com>
2025-06-15 01:01:18 +0300

    property-page

--- src/pages/PropertyPage.tsx
+++ src/pages/PropertyPage.tsx
@@ -212,7 +212,7 @@ export const PropertyPage = () => {
                 </div>
             </AnimatedSection>
 
-            <AnimatedSection className="container">
+            <AnimatedSection className="container property-page__details">
                 <TitleAndText
                     heading="Comprehensive Pricing Details"
                     description="At Estatein, transparency is key. We want you to have a clear understanding of all costs associated with your property investment. Below, we break down the pricing for Seaside Serenity Villa to help you make an informed decision"
